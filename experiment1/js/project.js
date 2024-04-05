7a73bffa4b9812388cc94e949228b4b2ef49888b
Mako1688 <marcoogazvega@hotmail.com>
2024-04-04 18:56:06 -0700

    adding music generator to website

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Marco Ogaz-Vega
+// Date: 4/4/24
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
